d9358160f9413520793a8dc05f32d2674773f513
Gian <62362598+infoR96@users.noreply.github.com>
2024-11-07 13:41:00 -0500

    Cambios en RegisterForm para manejar peticiones en el despliegue

--- src/app/components/users/RegisterForm.tsx
+++ src/app/components/users/RegisterForm.tsx
@@ -29,7 +29,7 @@ const RegisterForm: React.FC = () => {
     const user = { name, email, password };
 
     try {
-      const response = await fetch("/api/users", {
+      const response = await fetch("https://ract-next.vercel.app/api/users", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
